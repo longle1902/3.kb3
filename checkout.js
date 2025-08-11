a8323bc593967e8c9f83af8372afb96d6d965bc9
longle1902 <longle19022000@gmail.com>
2025-08-11 17:04:40 +0700

    new ui

--- checkout.js
+++ checkout.js
@@ -1,10 +1,10 @@
 function checkout() {
-  return false; // ❌ BUG: luôn trả về false
+  return true; // ✅ FIXED
+  console.log("Thanh toán thành công"); // thêm dòng này
 }
 
 if (!checkout()) {
-  console.log("❌ Thanh toán thất bại"); // bug hiện rõ
+  console.log("❌ Thanh toán thất bại");
 } else {
   console.log("✅ Thanh toán thành công");
 }
-
